8a1ff53dc797d2975fb5fd3bf615b2a51fe9d2ee
oscar <29248751+oscar-wos@users.noreply.github.com>
2024-06-07 18:36:50 +0100

    INIT

---
+++ src/Config.cs
@@ -0,0 +1,13 @@
+﻿using CounterStrikeSharp.API.Core;
+using System.Text.Json.Serialization;
+
+namespace Refiller;
+
+public class RefillerConfig : BasePluginConfig
+{
+    public override int Version { get; set; } = 1;
+    [JsonPropertyName("AssistRefill")] public bool AssistRefill { get; set; } = true; // true, false
+    [JsonPropertyName("HealthRefill")] public string HealthRefill { get; set; } = "all"; // "all", "0", "100"
+    [JsonPropertyName("AmmoRefill")] public string AmmoRefill { get; set; } = "all"; // "all", "current", "off"
+    [JsonPropertyName("ArmorRefill")] public bool ArmorRefill { get; set; } = true; // true, false
+}
---
+++ src/Globals.cs
@@ -0,0 +1,9 @@
+namespace Refiller;
+
+public partial class Refiller
+{
+    public RefillerConfig Config { get; set; } = new();
+    public override string ModuleName => "Refiller";
+    public override string ModuleAuthor => "github.com/oscar-wos/Refiller";
+    public override string ModuleVersion => "1.0.0";
+}
