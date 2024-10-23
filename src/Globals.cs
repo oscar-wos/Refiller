fd2501a9afaa37bea95e204efc85197557935110
Oscar <29248751+oscar-wos@users.noreply.github.com>
2024-10-23 08:32:48 +0100

    fix: invalid object reference

--- src/Globals.cs
+++ src/Globals.cs
@@ -5,5 +5,5 @@ public partial class Refiller
     public RefillerConfig Config { get; set; } = new();
     public override string ModuleName => "Refiller";
     public override string ModuleAuthor => "github.com/oscar-wos/Refiller";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "1.0.1";
 }
